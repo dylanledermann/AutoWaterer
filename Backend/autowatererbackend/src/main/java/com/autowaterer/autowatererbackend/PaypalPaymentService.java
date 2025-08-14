a7fc71461baf7127cb905d7497964bdfb1b65d1f
Dylan Ledermann <dylhead2003@gmail.com>
2025-08-13 23:54:44 -0700

    Removed unused package in PaypalPaymentService

--- Backend/autowatererbackend/src/main/java/com/autowaterer/autowatererbackend/PaypalPaymentService.java
+++ Backend/autowatererbackend/src/main/java/com/autowaterer/autowatererbackend/PaypalPaymentService.java
@@ -1,7 +1,5 @@
 package com.autowaterer.autowatererbackend;
 
-import org.springframework.stereotype.Component;
-
 public class PaypalPaymentService implements PaymentService {
     @Override
     public void processPayment(double amount){
