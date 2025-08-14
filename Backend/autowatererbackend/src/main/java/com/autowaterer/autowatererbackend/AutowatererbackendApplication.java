cbf3619c9430e190bd255078aabe61684399a0bd
dylhead <dylhead2003@gmail.com>
2025-08-13 17:37:20 -0700

    Readme and project setup including frontend and backend for autowaterer. Backend has initial Spring framework created, but no changes have been made yet

---
+++ Backend/autowatererbackend/src/main/java/com/autowaterer/autowatererbackend/AutowatererbackendApplication.java
@@ -0,0 +1,13 @@
+package com.autowaterer.autowatererbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutowatererbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutowatererbackendApplication.class, args);
+	}
+
+}
---
+++ Backend/autowatererbackend/src/test/java/com/autowaterer/autowatererbackend/AutowatererbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autowaterer.autowatererbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutowatererbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
