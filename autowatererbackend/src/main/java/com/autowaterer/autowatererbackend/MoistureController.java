a0e704a0b009eca413ca1d823cf31184727192f2
Dylan Ledermann <dylhead2003@gmail.com>
2025-08-14 23:42:49 -0700

    Changed moisture controller to send a number/int as a response to the get

--- autowatererbackend/src/main/java/com/autowaterer/autowatererbackend/MoistureController.java
+++ autowatererbackend/src/main/java/com/autowaterer/autowatererbackend/MoistureController.java
@@ -1,10 +1,11 @@
 package com.autowaterer.autowatererbackend;
 
-
 import org.json.JSONObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class MoistureController {
     
@@ -15,7 +16,7 @@ public class MoistureController {
     }
 
     @GetMapping("/api/moisture")
-    public String moisture(){
-        return "{\'Moisture\':\'" + Integer.toString(moistureObserver.getMoisture()) + "\'}";
+    public int moisture(){   
+        return moistureObserver.getMoisture();
     }
 }
