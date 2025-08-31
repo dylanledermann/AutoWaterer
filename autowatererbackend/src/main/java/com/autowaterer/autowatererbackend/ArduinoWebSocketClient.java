3dc1f01848c4d3d83b6d479fe0d38c1e50579677
Dylan Ledermann <dylhead2003@gmail.com>
2025-08-30 20:10:53 -0700

    Created reconnect for arduino websocket with to try and reconnect every minute up to 3 times

--- autowatererbackend/src/main/java/com/autowaterer/autowatererbackend/ArduinoWebSocketClient.java
+++ autowatererbackend/src/main/java/com/autowaterer/autowatererbackend/ArduinoWebSocketClient.java
@@ -17,23 +17,44 @@ import jakarta.annotation.PostConstruct;
 public class ArduinoWebSocketClient extends WebSocketClient {
     // Logger to log messages
     private static final Logger logger = LoggerFactory.getLogger(ArduinoWebSocketClient.class);
+    private final MoistureLogger mLog;
     private final MoistureObserver moistureObserver;
-    private final FrontendWebSocketServer ws;
+    private final int MAX_RECONNECTS = 3;
+    private int reconnects = 0;
+    private final String ArduinoIP;
+    private final String ArduinoPort;
 
-    public ArduinoWebSocketClient(@Value("${app.arduino.ip}") String ArduinoIP, @Value("${app.arduino.port}") String ArduinoPort, MoistureObserver moistureObserver, FrontendWebSocketServer ws) throws URISyntaxException {
+    public ArduinoWebSocketClient(@Value("${app.arduino.ip}") String ArduinoIP, @Value("${app.arduino.port}") String ArduinoPort, MoistureLogger mLog, MoistureObserver moistureObserver) throws URISyntaxException {
         super(new URI("ws://" + ArduinoIP + ":" + ArduinoPort));
+        this.ArduinoIP = ArduinoIP;
+        this.ArduinoPort = ArduinoPort;
+        this.mLog = mLog;
         this.moistureObserver = moistureObserver;
-        this.ws = ws;
     }
 
     @Override
     public void onOpen(ServerHandshake handshake){
         logger.info("Connected to Arduino");
+        this.reconnects = 0;
     }
 
     @Override
     public void onClose(int code, String reason, boolean remote){
         logger.info("Connection to arduino close: {} - {}", code, reason);
+        try{
+            if(this.reconnects < MAX_RECONNECTS){
+                Thread.sleep(60000);
+                this.reconnects++;
+                logger.info("Attempting to reconnect");
+                ArduinoWebSocketClient ws = new ArduinoWebSocketClient(this.ArduinoIP, this.ArduinoPort, this.mLog, this.moistureObserver);
+                ws.connect();
+            }
+            else{
+                logger.info("Max amount of reconnects occured");
+            }
+        } catch (Exception e){
+            logger.error("Reconnect error: ", e);
+        }
     }
 
     @Override
@@ -41,8 +62,8 @@ public class ArduinoWebSocketClient extends WebSocketClient {
         JSONObject json = new JSONObject(message);
         int newMoisture = json.getInt("Moisture");
         if(newMoisture != moistureObserver.getMoisture()){
+            this.mLog.logMoisture(newMoisture);
             moistureObserver.setMoisture(newMoisture);
-            ws.broadcastMoisture();
         }
     }
 
